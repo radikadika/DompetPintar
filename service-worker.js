bcb4ac04fe60edec6533b04de327f1c0347392ea
radikadika <radika.rahmanul7@guru.sma.belajar.id>
2026-01-17 12:48:27 +0700

    Update service worker cache version to v1.0.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dompet-pintar-v1.0.1';
+const CACHE_NAME = 'dompet-pintar-v1.0.2';
 const urlsToCache = [
   './index.html',
   './icon.png',
@@ -47,4 +47,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
+
 });
